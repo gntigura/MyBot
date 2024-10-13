c02fda9e0698ecb3c3cff4551a0c919c5c373c7d
Yann Ntigura <ganza.ntigura@student.uclouvain.be>
2024-10-13 23:02:23 +0200

    Ajout de fonctionalités

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ import discord
 from discord.ext import commands
 from weather import get_weather
 from latex import render_latex_to_image
+import random as r
+from datetime import datetime
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="/", intents=intents) 
+bot = commands.Bot(command_prefix="!", intents=intents) 
 
 @bot.event
 async def on_ready():
@@ -18,7 +20,7 @@ async def ping(ctx):
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f"Hello, {ctx.author.name}!")
+    await ctx.send(f"Hello, {ctx.member.displayName}!\n Today we are the {datetime.now()}!")
 
 @bot.command()
 async def weather(ctx, country):
@@ -43,5 +45,18 @@ async def latex(ctx, *, latex_code):
     except Exception as e:
         await ctx.send("{} while producing the latex".format(e))
 
+@bot.command()
+async def random(ctx, *, number):
+    try:
+        nbr = int(number)
+        if (number == 0):
+            await ctx.send("I can't choose a random number in a range of 0")
+            return
+        newn = r.choice(nbr)
+        await ctx.send("I choose number {}".format(newn))
+        return
+    except Exception:
+        await ctx.send("This is not a number {}".format(number))
+        return
 
-bot.run("MTI5NTA0MjY1NTIwMTM5ODc4NA.Gs4neo.cD2rStjf3tVSnxkbWzNxdhW7jStzC6eHyy57cI")
+#bot.run("MTI5NTA0MjY1NTIwMTM5ODc4NA.Gs4neo.cD2rStjf3tVSnxkbWzNxdhW7jStzC6eHyy57cI")
