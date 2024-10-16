227bf49c7159aa6d06bb6bc88920233252c8b1fb
Yann Ntigura <ganza.ntigura@student.uclouvain.be>
2024-10-16 22:19:51 +0200

    partially fixed some of the bot problems

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from latex import render_latex_to_image
 import random as r
 from datetime import datetime
 import asyncio
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,39 +17,43 @@ async def on_ready():
 
 @bot.command()
 async def help_me(ctx):
-    response = "ShhhBot has different commands \n !bum : to find who's the bum \n !weather location : to give the weather of a location \n !latex message :  to compile a message into LaTex (might not work well, still in developpement) \n !random number : lets the bot choose a random number from range(0,number) (Developping this command to create a small guessing game) \n !Shhh : the bot will send a famous picture"
+    response = "ShhhBot has different commands \n !bum : to find who's the bum \n !weather location : to give the weather of a location \n !latex message :  to compile a message into LaTex (might not work well, still in developpement) \n !random number : lets the bot choose a random number from range(0,number) (Developping this command to create a small guessing game) \n !Shhh : the bot will send a famous picture \n !hello: the bot will greet to the author of the message"
+    await ctx.send("You thought I was here to help you? :joy_cat: :thumbsdown:")
+    await asyncio.sleep(10.0)
+    await ctx.send("I'm just kidding")
     await ctx.send(response)
 
 @bot.command()
-@commands.has_permissions(ban_members= True)
-async def bum(ctx, message: str, second: int):
+async def bum(message):
 
 
     #So the bot doesn't respond to himself
-    if ctx.author.id == bot.user.id:
+    if message.author.id == bot.user.id:
         return
+    if message.content.startswith("!bum"):
+        await message.send("Who's the bum? You got 10 seconds to respond (be careful on what you respond)")
 
-    await ctx.send("Who's the bum (enter Yann, Samy or Felix)? You got 10 seconds to respond (be careful on what you respond)")
-    def check(m):
-        members = ['Yann', 'Samy', 'Felix']
-        return  m.author == ctx.author and m.content in members
+        def check(m):
+            return  m.author == message.author
     
-    try:
-        response = bot.wait_for('message', check=check,timeout=10.0)
-    except asyncio.TimeoutError :
-        return await ctx.send("{} must really be a bum if it took you more than 10 seconds to make a choice".format(ctx.author.name))
-    if str(response).upper() == "YANN":
-        await ctx.send("The Boy isn't a bum, he is your savior!")
-        await ctx.send("ShhhBot has the permissions to ban users, so be careful of not spamming this command ;)")
-        return
-    await ctx.send("{} IS A BUM!!".format(str(response).upper()))
-    return
+        try:
+            response = bot.wait_for('message', check=check,timeout=1000.0)
+        except asyncio.TimeoutError :
+            return await message.send("{} must really be a bum if it took you more than 10 seconds to make a choice".format(ctx.author.name))
+        
+        if response.upper() == "YANN":
+            await message.send("The Boy isn't a bum, he is your savior!")
+            await message.send("ShhhBot has the permissions to ban users, so be careful of not spamming this command ;)")
+            return
+        else:
+            await message.send("{} IS A BUM!!".format(response.upper()))
+            return
 
 
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f"Hello, {ctx.author.name}!\n Today we are the {datetime.now()}!")
+    await ctx.send("I ain't no bum Bot sayin hello :joy_cat: :thumbsdown:")
 
 @bot.command()
 async def weather(ctx, *, country):
@@ -63,12 +68,12 @@ async def weather(ctx, *, country):
     await ctx.send(response)
 
 @bot.command()
-async def latex(ctx, *, latex_code):
+async def latex(ctx, *, latex_code: str):
     try:
         image = render_latex_to_image(latex_code)
 
-        file = discord.File(image, filename="latex.png")
-        await ctx.send(file)
+        buf = discord.File(image, filename="latex.png")
+        await ctx.send(file=buf)
         return
     except Exception as e:
         await ctx.send("{} while producing the latex".format(e))
@@ -88,15 +93,16 @@ async def random(ctx, *, number: int):
     
 @bot.command()
 async def Shhh(ctx):
-    with open("IMG_6700.JPG", 'rb') as f:
-        image = discord.File(f)
-        await ctx.send(image)
+    paths = os.path.join('/Users', 'yann', 'Desktop', 'MyBot', 'MyBot', 'src', 'IMG_6700.JPG')
+    if os.path.exists(paths):
+        await ctx.send(file=discord.File(paths))
         return
+    await ctx.send("The famous picture is lost :joy_cat: :thumbsdown:")
 
 def main():
     #No need to run the code, this is not the right token
-    
-    #bot.run("MTI5NTA0MjY1NTIwMTM5ODc4NA.GaTwS_.3-zdDKhkgBoJ6rI-do87vbtS0jzMUqbZQ8")
+
+    #bot.run("MTI5NTA0MjY1NTIwMTM5ODc4NA.Gpy24u.dawQjAEx1C0JX4vxPlxhvORHh2BmZYMK9sjojQ")
     return
 
 if __name__ == "__main__":
